f3e346301f9e2d9affe425279d5accc2c9e29fb9
AvikRay1001 <avikray1001@gmail.com>
2025-05-22 21:16:13 +0530

    Day3

--- pages/api/utils.ts
+++ pages/api/utils.ts
@@ -1,6 +1,8 @@
-import { Pinecone } from "@pinecone-database/pinecone";
+import { Pinecone, PineconeRecord, RecordMetadata } from "@pinecone-database/pinecone";
 import { Document } from "langchain/document";
 import { FeatureExtractionPipeline, pipeline } from "@xenova/transformers";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import { batchSize } from "@/config";
 
 export async function updateVectorDB(
     client: Pinecone,
@@ -19,6 +21,48 @@ export async function updateVectorDB(
     }
 }
 
-function processDocument(client: Pinecone, indexname: string, namespace: string, doc: Document<Record<string, any>>, extractor: FeatureExtractionPipeline) {
-    console.log(doc)
+async function processDocument(client: Pinecone, indexname: string, namespace: string, doc: Document<Record<string, any>>, extractor: FeatureExtractionPipeline) {
+    const splitter = new RecursiveCharacterTextSplitter();
+    const documentChunks = await splitter.splitText(doc.pageContent);
+    const filename = getFileName(doc.metadata.source);
+
+    console.log(documentChunks.length);
+    let chunkBatchIndex = 0;
+    while(documentChunks.length > 0){
+        chunkBatchIndex++;
+        const chunkBatch = documentChunks.splice(0, batchSize)
+        await processOneBatch(client, indexname, namespace, extractor, chunkBatch, chunkBatchIndex, chunkBatchIndex, filename)
+    }
 }
+
+function getFileName(filename:string): string {
+    const docname = filename.substring(filename.lastIndexOf('/') + 1);
+    return docname.substring(0, docname.lastIndexOf('.')) || docname;
+}
+
+
+
+async function processOneBatch(client: Pinecone, indexname: string, namespace: string, extractor: FeatureExtractionPipeline, chunkBatch: string[], chunkBatchIndex: number, chunkBatchIndex1: number, filename: string) {
+    const output = await extractor(chunkBatch.map(str => str.replace(/\n/g, ' ')), {
+        pooling: 'cls'
+    });
+    const embeddingsBatch = output.tolist();
+    let vectorBatch: PineconeRecord<RecordMetadata>[] = [];
+    for(let i=0;i<chunkBatch.length;i++){
+        const chunk = chunkBatch[i];
+        const embedding = embeddingsBatch[i];
+        const vector: PineconeRecord<RecordMetadata> = {
+            id: `${filename}-${chunkBatchIndex}-${i}`,
+            values: embedding,
+            metadata: {
+                chunk
+            }
+        }
+        vectorBatch.push(vector);
+    }
+
+    const index = client.Index(indexname).namespace(namespace);
+    await index.upsert(vectorBatch);
+    vectorBatch = [];
+}
+
